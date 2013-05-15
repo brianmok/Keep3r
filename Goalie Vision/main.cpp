116e36c2785d8e1f7039ea7e918bf56a948e30c4
David Saltzman <davidbsaltzman@gmail.com>
2013-05-15 11:26:32 -0700

    Initial Commit

---
+++ Goalie Vision/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Goalie Vision
+//
+//  Created by David Saltzman on 5/15/13.
+//  Copyright (c) 2013 David Saltzman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
