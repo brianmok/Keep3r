5272ae72bfb5ecb98bf40c8d27450d20de7968b8
davidbsaltzman <davidbsaltzman@gmail.com>
2013-05-24 14:41:01 -0600

    commented main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include "RobotCom.h"
 #include "PrVector.h"
@@ -11,7 +10,10 @@ int main(int argc, char** argv)
   while (1)
   {
     float x_f = processFrame();
+    // at some point, this is probably multithreaded.
   }
+  
+  // currently, none of the code past here testing the robot actually runs, it just does the vision loop.
   
 	std::cout<<"This program tests the network connectivity between the client simulator and the servo server"<<std::endl;
 
@@ -26,6 +28,7 @@ int main(int argc, char** argv)
 //TEST 2: SEND MESSAGE TO THE SERVO SERVER
 /****************************************/
 	test_robot->jointControl( JMOVE, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+	// we'll probably actually use something like test_robot->control(TRACK, targetPosArray, 6) for operational space
 	
 
 /****************************************/
